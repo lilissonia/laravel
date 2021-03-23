783b3639af548991b5d94adaa5fa41506f9a4006
lilissonia <lilissonia31@gmail.com>
2021-03-23 14:34:12 +0700

    lilissonia

---
+++ resources/views/about.blade.php
@@ -0,0 +1,44 @@
+@extends('layout/main')
+
+@section('title', 'about min nanda!')
+
+@section('container')
+
+  <div class="container">
+    <div class="row">
+      <div class="col-10">
+        <div class="spinner-border text-primary" role="status">
+  <span class="sr-only">Loading...</span>
+</div>
+<div class="spinner-border text-secondary" role="status">
+  <span class="sr-only">Loading...</span>
+</div>
+<div class="spinner-border text-success" role="status">
+  <span class="sr-only">Loading...</span>
+</div>
+<div class="spinner-border text-danger" role="status">
+  <span class="sr-only">Loading...</span>
+</div>
+<div class="spinner-border text-warning" role="status">
+  <span class="sr-only">Loading...</span>
+</div>
+<div class="spinner-border text-info" role="status">
+  <span class="sr-only">Loading...</span>
+</div>
+<div class="spinner-border text-dark" role="status">
+  <span class="sr-only">Loading...</span>
+</div>
+      <div class="card">
+  <h5 class="card-header">Social Media</h5>
+  <div class="card-body">
+    <h5 class="card-title">LILIS SONIA</h5>
+    <p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
+    <a href="https://twitter.com/ciminies" class="btn btn-dark">Twitter</a>
+    <a href="https://www.instagram.com/risnandahm/" class="btn btn-warning">instagram</a>
+  </div>
+</div>
+      </div>
+    </div>
+  </div>
+
+@endsection
---
+++ resources/views/index.blade.php
@@ -0,0 +1,22 @@
+@extends('layout/main')
+
+@section('title', 'LILIS SONIA')
+
+@section('container')
+
+	<div class="container">
+		<div class="row">
+			<div class="col-10">
+				<div class="card">
+  <div class="card-header">
+    profil
+  </div>
+	<h1 class="mt-2">Hello, Lilis Sonia !!</h1>
+<object width="555" height="300" data="https://youtube.com/v/i-5_It0T3z4" type="application/x-shockwave-flash"><param name="src"value="http://www.youtube.com/v/PV1gCvzpSy0"></object></div>
+  <div class="card-body">
+    <h5 class="card-title">  18090051 | kelas 6c</h5>
+		</div>
+	</div>
+	 </div>
+</div>
+@endsection
